6b76f1a1f598584d65342ca26822324bab7f81ec
Pruthvi <45816639+Pruthvi-Sanghavi@users.noreply.github.com>
2020-03-29 18:31:37 -0400

    images cropped

---
+++ crop_images.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+if __name__ == '__main__':
+    # Read image
+    im = cv2.imread("data/frame170.png")
+
+    # Select ROI
+    showCrosshair = False
+    fromCenter = False
+    r = cv2.selectROI("Image", im, fromCenter, showCrosshair)
+
+    # Crop image
+    imCrop = im[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]
+
+    # Display cropped image
+    cv2.imshow("Image", imCrop)
+    cv2.imwrite("cropped_imagesY/frame0.png", imCrop )
+    cv2.waitKey(0)
