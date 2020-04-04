7d055e321b1a902b6916e9094cbfa6b0c3672c30
Pruthvi-Sanghavi <pruthvi@umd.edu>
2020-04-04 17:45:21 -0400

    updated data reading

--- segRed_1d.py
+++ segRed_1d.py
@@ -10,9 +10,18 @@ def calcGaussian(pixel, mean, stddev):
     N = (1/((2*np.pi*np.linalg.det(stddev))**(1/2)))*math.exp((-1/2)*((pixel - mean).T) *(np.linalg.inv(stddev)) * (pixel - mean))
     return N
 
-def createData(N, image):
+def createData(N):
     pixel = []
-    for i in range()
-    image = (cv2.imread(file) for file in glob.glob("Red_DataSet/Crop_segmented/Train/*.png"))
+    for x in range(N):
+        image = cv2.imread("Red_DataSet/Crop_segmented/Train/frame%d.png" %x)
+        for i in range(image.shape[0]):
+            for j in range(image.shape[1]):
+                if image[i,j,2] > 50:
+                    pixel = np.append([pixel], [image[i,j,2]])
+                    print(pixel)
+        # print(image.shape[0])
+
+createData(118)
+print('done')
 
 
